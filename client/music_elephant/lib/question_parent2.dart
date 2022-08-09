ce16cbe228d9a99944c6ad67fed83001436debdf
michaelmjh <74016689+michaelmjh@users.noreply.github.com>
2022-08-09 16:35:08 +0100

    arrange working-ish

--- client/music_elephant/lib/question_parent2.dart
+++ client/music_elephant/lib/question_parent2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:music_elephant/answer_widget2.dart';
+import 'package:music_elephant/question_widget2.dart';
 
 import 'QuestionAssets/question_assets.dart';
 
@@ -16,29 +18,18 @@ class _QuestionState extends State<QuestionParent2> {
     null;
   }
 
-  void updateDroppedItems(data) {
-    print(data);
-    if (data == answerOptions[0]) {
-      _firstDropped = true;
-    } else if (data == answerOptions[1]) {
-      _secondDropped = true;
-    } else if (data == answerOptions[2]) {
-      _thirdDropped = true;
-    } else if (data == answerOptions[3]) {
-      _fourthDropped = true;
+  var answerOptions = ["A", "B", "C", "D"];
+  var submittedAnswer = [];
+  var completeAnswer = false;
+
+  void updateAnswer(data) {
+    submittedAnswer.add(data);
+    if (submittedAnswer.length == answerOptions.length) {
+      setState(() {
+        completeAnswer = true;
+      });
     }
   }
-
-  var answerOptions = ["A", "B", "C", "D"];
-
-  var _droppedInFirstPosition = false;
-  var _droppedInSecondPosition = false;
-  var _droppedInThirdPosition = false;
-  var _droppedInFourthPosition = false;
-  var _firstDropped = false;
-  var _secondDropped = false;
-  var _thirdDropped = false;
-  var _fourthDropped = false;
 
   @override
   Widget build(BuildContext context) {
@@ -49,138 +40,23 @@ class _QuestionState extends State<QuestionParent2> {
       body: Column(
         children: [
           Row(
-            children: [
-              Expanded(
-                child: DragTarget<String>(
-                  builder: (
-                    BuildContext context,
-                    List<dynamic> accepted,
-                    List<dynamic> rejected,
-                  ) {
-                    return Container(
-                      height: 120.0,
-                      width: 120.0,
-                      child: Center(
-                        child: Text(_droppedInFirstPosition ? 'filled' : ' '),
-                      ),
-                    );
-                  },
-                  onAccept: (data) {
-                    setState(() {
-                      updateDroppedItems(data);
-                      _droppedInFirstPosition = true;
-                    });
-                  },
-                ),
-              ),
-              Expanded(
-                child: DragTarget<String>(
-                  builder: (
-                    BuildContext context,
-                    List<dynamic> accepted,
-                    List<dynamic> rejected,
-                  ) {
-                    return Container(
-                      child: Center(
-                        child: Image.asset(_droppedInSecondPosition
-                            ? 'images/dog-png-30.png'
-                            : 'images/Square.png'),
-                      ),
-                    );
-                  },
-                  onAccept: (data) {
-                    setState(() {
-                      updateDroppedItems(data);
-                      _droppedInSecondPosition = true;
-                    });
-                  },
-                ),
-              ),
-              Expanded(
-                child: DragTarget<String>(
-                  builder: (
-                    BuildContext context,
-                    List<dynamic> accepted,
-                    List<dynamic> rejected,
-                  ) {
-                    return Container(
-                      child: Center(
-                        child: Image.asset(_droppedInThirdPosition
-                            ? 'images/dog-png-30.png'
-                            : 'images/Square.png'),
-                      ),
-                    );
-                  },
-                  onAccept: (data) {
-                    setState(() {
-                      updateDroppedItems(data);
-                      _droppedInThirdPosition = true;
-                    });
-                  },
-                ),
-              ),
-              Expanded(
-                child: DragTarget<String>(
-                  builder: (
-                    BuildContext context,
-                    List<dynamic> accepted,
-                    List<dynamic> rejected,
-                  ) {
-                    return Container(
-                      child: Center(
-                        child: Image.asset(_droppedInFourthPosition
-                            ? 'images/dog-png-30.png'
-                            : 'images/Square.png'),
-                      ),
-                    );
-                  },
-                  onAccept: (data) {
-                    setState(() {
-                      updateDroppedItems(data);
-                      _droppedInFourthPosition = true;
-                    });
-                  },
-                ),
-              ),
-            ],
+            children: answerOptions.map((answer) {
+              return QuestionWidget2(updateAnswer);
+            }).toList(),
           ),
           Row(
-            children: answerOptions.map((option) {
-              return (Visibility(
-                visible: !_firstDropped,
-                child: Expanded(
-                  child: Draggable<String>(
-                    data: option,
-                    child: Container(
-                      color: Colors.blue,
-                      height: 120.0,
-                      width: 120.0,
-                      child: Center(
-                        child: Text(
-                          option,
-                        ),
-                      ),
-                    ),
-                    feedback: Container(
-                      height: 120.0,
-                      width: 120.0,
-                      child: Center(
-                        child: Text(
-                          option,
-                        ),
-                      ),
-                    ),
-                    childWhenDragging: Container(
-                      height: 120.0,
-                      width: 120.0,
-                      child: Center(
-                        child: Icon(Icons.cancel_presentation_outlined),
-                      ),
-                    ),
-                  ),
-                ),
-              ));
+            children: answerOptions.map((answer) {
+              return AnswerWidget2(answer);
             }).toList(),
+          ),
+          Visibility(
+            visible: completeAnswer,
+            child: ElevatedButton(
+              child: Text("submit"),
+              onPressed: (() {
+                print("hello is me youre looking for");
+              }),
+            ),
           ),
         ],
       ),
