d401b9636d12623fc41ac6bf0a6ea688edcab3d5
codeclanstudent <ewan.cruickshank91@googlemail.com>
2022-08-09 16:32:13 +0100

    Server Side

---
+++ server/src/main/java/com/example/server/models/Lesson.java
@@ -0,0 +1,70 @@
+package com.example.server.models;
+
+import com.example.server.models.enums.LessonName;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="lessons")
+
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private LessonName lessonName;
+
+    @ManyToOne
+    @JoinColumn(name="level_id", nullable = false)
+    @JsonIgnoreProperties({"lessons"})
+    private Level level;
+
+    private List<String> slides;
+
+    public Lesson(LessonName lessonName, Level level){
+        this.lessonName = lessonName;
+        this.level = level;
+        this.slides = new ArrayList<>();
+    }
+
+    public Lesson(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LessonName getLessonName() {
+        return lessonName;
+    }
+
+    public void setLessonName(LessonName lessonName) {
+        this.lessonName = lessonName;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public List<String> getSlides() {
+        return slides;
+    }
+
+    public void setSlides(List<String> slides) {
+        this.slides = slides;
+    }
+}
---
+++ server/src/main/java/com/example/server/models/User.java
@@ -0,0 +1,65 @@
+package com.example.server.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String userName;
+
+    @Column
+    private String avatar;
+
+    @Column
+    private List<Enum> progress;
+
+    public User(String userName, String avatar){
+        this.userName = userName;
+        this.avatar = avatar;
+        this.progress = new ArrayList<>();
+    }
+
+    public User(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public List<Enum> getProgress() {
+        return progress;
+    }
+
+    public void setProgress(List<Enum> progress) {
+        this.progress = progress;
+    }
+}
