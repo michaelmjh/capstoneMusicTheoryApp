f49824c9f55adc4642b268c14e055e6cad2044e7
codeclanstudent <ewan.cruickshank91@googlemail.com>
2022-08-10 10:51:02 +0100

    created User repo

--- server/src/main/java/com/example/server/repositories/UserRepository.java
+++ server/src/main/java/com/example/server/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.server.repositories;
 
-public interface UserRepository {
+import com.example.server.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
