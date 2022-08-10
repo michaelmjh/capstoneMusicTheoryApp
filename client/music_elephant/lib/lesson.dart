c2ed9953b735147421815e1796d5db6b4f0fdf66
Nick Jones <nickj_24@hotmail.com>
2022-08-10 14:37:59 +0100

    navigation buttons on swiper

--- client/music_elephant/lib/lesson.dart
+++ client/music_elephant/lib/lesson.dart
@@ -9,42 +9,70 @@ class Lesson extends StatefulWidget {
 }
 
 class _LessonState extends State<Lesson> {
-  List<String> images = [
-    "client/music_elephant/images/lessonImages/puppy1.png",
-    "client/music_elephant/images/lessonImages/puppy2.png",
-    "client/music_elephant/images/lessonImages/puppy3.png",
-    "client/music_elephant/images/lessonImages/puppy4.png",
-    "client/music_elephant/images/lessonImages/puppy5.png"
+  List images = [
+    "images/lessonImages/puppy1.png",
+    "images/lessonImages/puppy2.png",
+    "images/lessonImages/puppy3.png",
+    "images/lessonImages/puppy4.png"
   ];
 
+  void nextPage(Function({bool animation  true}) next
+
+  void previousPage() {
+    Image.asset(images[0 - 1]);
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('lesson'),
+        title: const Text('lesson'),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.home),
+            icon: const Icon(Icons.home),
             onPressed: () {
               Navigator.pop(context);
             },
           ),
         ],
       ),
-      body: Swiper(
-        itemCount: images.length,
-        loop: false,
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(27.0),
-            child: Image.asset(
-              images[index],
-            ),
-          );
-        },
-        itemWidth: 200.00,
-        itemHeight: 300.00,
-        layout: SwiperLayout.STACK,
+      body: Column(
+        children: [
+          Swiper(
+            itemCount: images.length,
+            loop: false,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(27.0),
+                child: Image.asset(images[index]),
+              );
+            },
+            itemWidth: 200.00,
+            itemHeight: 300.00,
+            layout: SwiperLayout.STACK,
+            pagination: const SwiperPagination(),
+            control: const SwiperControl(iconPrevious: Icons.arrow_back_ios,  iconNext: Icons.arrow_forward_ios),
+          ),
+          // ListTile(
+          //   title: Row(
+          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //     children: <Widget>[
+          //       Expanded(
+          //         child: ElevatedButton(
+          //           onPressed: nextPage,
+          //           child: const Text('next page'),
+          //         ),
+          //       ),
+          //       Expanded(
+          //         child: ElevatedButton(
+          //           onPressed: SwiperController( nextPage({bool animation: true}),),
+          //           child: const Text('previous page'),
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
+        ],
       ),
     );
   }
