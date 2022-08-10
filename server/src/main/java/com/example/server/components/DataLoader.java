70fe9737069168f29f0f17f8c071da55c20127ce
codeclanstudent <ewan.cruickshank91@googlemail.com>
2022-08-10 14:55:31 +0100

    Added a dataloader with dummy data and set up getAll routes for Lessons and Levels and it works

---
+++ server/src/main/java/com/example/server/components/DataLoader.java
@@ -0,0 +1,49 @@
+package com.example.server.components;
+
+import com.example.server.models.Lesson;
+import com.example.server.models.Level;
+import com.example.server.models.enums.LessonName;
+import com.example.server.models.enums.LevelName;
+import com.example.server.repositories.LessonRepository;
+import com.example.server.repositories.LevelRepository;
+import com.example.server.repositories.QuestionRepository;
+import com.example.server.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    LevelRepository levelRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    QuestionRepository questionRepository;
+    @Autowired
+    LessonRepository lessonRepository;
+
+    public DataLoader(){
+
+    }
+
+    public void run(ApplicationArguments args){
+        List lessonAssets = new ArrayList<>();
+        lessonAssets.add("the image is here");
+        lessonAssets.add("the other image is here");
+        Level level1 = new Level(LevelName.BEGINNER);
+        levelRepository.save(level1);
+        Lesson lesson1 = new Lesson(LessonName.SCALES1, level1);
+        lesson1.setSlides(lessonAssets);
+        lessonRepository.save(lesson1);
+        Lesson lesson2 = new Lesson(LessonName.CHORDS1, level1);
+        lessonRepository.save(lesson2);
+        Lesson lesson3 = new Lesson(LessonName.KEYSIGNATURES1, level1);
+        lessonRepository.save(lesson3);
+    }
+}
--- server/src/main/java/com/example/server/controllers/LessonController.java
+++ server/src/main/java/com/example/server/controllers/LessonController.java
@@ -3,12 +3,20 @@ package com.example.server.controllers;
 import com.example.server.models.Lesson;
 import com.example.server.repositories.LessonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
 public class LessonController {
 
+    @Autowired
+    LessonRepository lessonRepository;
 
+    @GetMapping(value = "/lessons")
+    public ResponseEntity<List<Lesson>> getAllLessons(){
+        return new ResponseEntity<>(lessonRepository.findAll(), HttpStatus.OK);
+    }
 }
