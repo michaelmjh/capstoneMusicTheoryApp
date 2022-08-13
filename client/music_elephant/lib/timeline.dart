97b14c5939eab63db41aef93e3c1268fe3106500
shunaMcCallum <shuna.mccallum@gmail.com>
2022-08-13 12:06:14 +0100

    styling and layout updated

--- client/music_elephant/lib/timeline.dart
+++ client/music_elephant/lib/timeline.dart
@@ -1,9 +1,6 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:music_elephant/journey.dart';
 import 'package:music_elephant/landing_page.dart';
-// import 'package:timeline_tile/timeline_tile.dart';
 import 'package:timelines/timelines.dart';
 
 import 'dart:math';
@@ -43,26 +40,17 @@ class _TimelineState extends State<Timeline> {
     }
   }
 
-  Lesson dummyBegStart =
+  Lesson dummyBeg =
       Lesson(name: "DummyBeginner", slides: [], level: Level.beginner);
-  Lesson dummyBegEnd =
-      Lesson(name: "DummyBegEnd", slides: [], level: Level.beginner);
-  Lesson dummyIntStart =
+  Lesson dummyInt =
       Lesson(name: "DummyIntermediate", slides: [], level: Level.intermediate);
-  Lesson dummyIntEnd =
-      Lesson(name: "DummyIntEnd", slides: [], level: Level.intermediate);
   Lesson dummyAdvStart =
       Lesson(name: "DummyAdvanced", slides: [], level: Level.advanced);
-  Lesson dummyAdvEnd =
-      Lesson(name: "DummyAdvEnd", slides: [], level: Level.advanced);
 
   void setNewList() {
-    begList.insert(0, dummyBegStart);
-    begList.add(dummyBegEnd);
-    intList.insert(0, dummyIntStart);
-    intList.add(dummyIntEnd);
+    begList.insert(0, dummyBeg);
+    intList.insert(0, dummyInt);
     advList.insert(0, dummyAdvStart);
-    advList.add(dummyAdvEnd);
     for (var item in begList) {
       newList.add(item);
     }
@@ -120,13 +108,13 @@ class _Timeline1 extends StatelessWidget {
     return Flexible(
       child: FixedTimeline.tileBuilder(
         theme: TimelineThemeData(
-          nodePosition: 0.05,
+          nodePosition: 0.03,
           connectorTheme: ConnectorThemeData(
-            thickness: 8.0,
+            thickness: 4.0,
             color: Color(0xffd3d3d3),
           ),
           indicatorTheme: IndicatorThemeData(
-            size: 45.0,
+            size: 35.0,
           ),
         ),
         builder: TimelineTileBuilder.connected(
@@ -134,40 +122,41 @@ class _Timeline1 extends StatelessWidget {
             if (data[index].level == Level.beginner) {
               return _EmptyContentsBeginner(data[index], completedQuizzes);
             } else if (data[index].level == Level.intermediate) {
-              return _EmptyContentsInt(data[index]);
+              return _EmptyContentsInt(data[index], completedQuizzes);
             } else if (data[index].level == Level.advanced) {
-              return _EmptyContentsAdv(data[index]);
+              return _EmptyContentsAdv(data[index], completedQuizzes);
             }
           },
           connectorBuilder: (_, index, __) {
             if (index == 0 || completedLessons.contains(data[index])) {
-              return SolidLineConnector(color: Color(0xff6ad192));
+              return SolidLineConnector(color: Colors.pink);
             } else if (data[index].name == "DummyBeginner") {
-              return SolidLineConnector(color: Color(0xff6ad192));
+              return SolidLineConnector(color: Colors.pinkAccent);
             } else {
               return SolidLineConnector();
             }
           },
           indicatorBuilder: (_, index) {
             if (data[index].name == "DummyBeginner") {
-              return DotIndicator(color: Colors.yellow);
+              return DotIndicator(color: Colors.pink);
             } else if (data[index].name == "DummyBegEnd") {
               return null;
             } else if (data[index].name == "DummyIntermediate") {
-              return DotIndicator(color: Colors.orange);
+              return DotIndicator(color: Color.fromARGB(255, 12, 194, 175));
             } else if (data[index].name == "DummyIntEnd") {
               return null;
             } else if (data[index].name == "DummyAdvanced") {
-              return DotIndicator(color: Colors.teal);
+              return DotIndicator(color: Color.fromARGB(255, 190, 94, 207));
             } else if (data[index].name == "DummyAdvEnd") {
               return null;
             } else {
               switch (completedLessons.contains(data[index])) {
                 case true:
                   return DotIndicator(
-                    color: Color(0xff6ad192),
-                    child: ElevatedButton(
-                      child: Icon(Icons.star),
+                    color: Colors.pink,
+                    child: IconButton(
+                      icon: Icon(Icons.check, size: 20.0),
+                      color: Colors.white,
                       onPressed: () {
                         setSelectedLesson(data[index]);
                         Navigator.pushNamed(context, '/landingpage');
@@ -183,7 +172,7 @@ class _Timeline1 extends StatelessWidget {
               }
             }
           },
-          itemExtentBuilder: (_, __) => 200.0,
+          itemExtentBuilder: (_, __) => 150.0,
           itemCount: data.length,
         ),
       ),
@@ -202,40 +191,56 @@ class _EmptyContentsBeginner extends StatelessWidget {
     if (listItem.name == "DummyBeginner") {
       return Container(
           height: 40.0,
-          child: Text("Beginner Lessons", style: TextStyle(fontSize: 30.0)));
-    } else if (listItem.name == "DummyBegEnd") {
-      return Divider(color: Colors.black, indent: 20.0, endIndent: 20.0);
+          margin: EdgeInsets.only(left: 10.0),
+          child: Text("Beginner Lessons", style: TextStyle(fontSize: 25.0)));
     } else {
-      return Column(
-        children: [
-          Text(
-            listItem.name.toString(),
-            style: TextStyle(fontSize: 20.0),
-          ),
-          Row(children: [
-            Builder(builder: (__) {
-              if (completedQuizzes.length == 0) {
-                return Icon(Icons.star_border_outlined);
-              } else {
-                return Icon(Icons.star_border_outlined, color: Colors.yellow);
-              }
-            }),
-            Builder(builder: (__) {
-              if (completedQuizzes.length < 2) {
-                return Icon(Icons.star_border_outlined);
-              } else {
-                return Icon(Icons.star_border_outlined, color: Colors.yellow);
-              }
-            }),
-            Builder(builder: (__) {
-              if (completedQuizzes.length < 3) {
-                return Icon(Icons.star_border_outlined);
-              } else {
-                return Icon(Icons.star_border_outlined, color: Colors.yellow);
-              }
-            })
-          ]),
-        ],
+      return Card(
+        margin: EdgeInsets.all(40.0),
+        color: Color.fromARGB(255, 206, 199, 199),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            side: BorderSide(
+              color: Color.fromARGB(255, 111, 109, 109),
+            )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              listItem.name.toString(),
+              style: TextStyle(fontSize: 20.0),
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Builder(builder: (__) {
+                    if (completedQuizzes.length == 0) {
+                      return Icon(Icons.star_border_outlined);
+                    } else {
+                      return Icon(Icons.star_border_outlined,
+                          color: Colors.yellow);
+                    }
+                  }),
+                  Builder(builder: (__) {
+                    if (completedQuizzes.length < 2) {
+                      return Icon(Icons.star_border_outlined);
+                    } else {
+                      return Icon(Icons.star_border_outlined,
+                          color: Colors.yellow);
+                    }
+                  }),
+                  Builder(builder: (__) {
+                    if (completedQuizzes.length < 3) {
+                      return Icon(Icons.star_border_outlined);
+                    } else {
+                      return Icon(Icons.star_border_outlined,
+                          color: Colors.yellow);
+                    }
+                  })
+                ]),
+          ],
+        ),
       );
     }
   }
@@ -243,26 +248,65 @@ class _EmptyContentsBeginner extends StatelessWidget {
 
 class _EmptyContentsInt extends StatelessWidget {
   final listItem;
+  final completedQuizzes;
 
-  const _EmptyContentsInt(this.listItem);
+  const _EmptyContentsInt(this.listItem, this.completedQuizzes);
 
   @override
   Widget build(BuildContext context) {
     if (listItem.name == "DummyIntermediate") {
       return Container(
           height: 40.0,
+          margin: EdgeInsets.only(left: 10.0),
           child:
-              Text("Intermediate Lessons", style: TextStyle(fontSize: 30.0)));
-    } else if (listItem.name == "DummyIntEnd") {
-      return Divider(color: Colors.black, indent: 20.0, endIndent: 20.0);
+              Text("Intermediate Lessons", style: TextStyle(fontSize: 25.0)));
     } else {
-      return Container(
-        margin: EdgeInsets.only(left: 10.0),
-        height: 40.0,
-        child: Text(listItem.name.toString(), style: TextStyle(fontSize: 20.0)),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(2.0),
-          color: Colors.red,
+      return Card(
+        margin: EdgeInsets.all(40.0),
+        color: Color.fromARGB(255, 206, 199, 199),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            side: BorderSide(
+              color: Color.fromARGB(255, 111, 109, 109),
+            )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              listItem.name.toString(),
+              style: TextStyle(fontSize: 20.0),
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Builder(builder: (__) {
+                    if (completedQuizzes.length == 0) {
+                      return Icon(Icons.star_border_outlined);
+                    } else {
+                      return Icon(Icons.star_border_outlined,
+                          color: Colors.yellow);
+                    }
+                  }),
+                  Builder(builder: (__) {
+                    if (completedQuizzes.length < 2) {
+                      return Icon(Icons.star_border_outlined);
+                    } else {
+                      return Icon(Icons.star_border_outlined,
+                          color: Colors.yellow);
+                    }
+                  }),
+                  Builder(builder: (__) {
+                    if (completedQuizzes.length < 3) {
+                      return Icon(Icons.star_border_outlined);
+                    } else {
+                      return Icon(Icons.star_border_outlined,
+                          color: Colors.yellow);
+                    }
+                  })
+                ]),
+          ],
         ),
       );
     }
@@ -271,32 +315,66 @@ class _EmptyContentsInt extends StatelessWidget {
 
 class _EmptyContentsAdv extends StatelessWidget {
   final listItem;
+  final completedQuizzes;
 
-  const _EmptyContentsAdv(this.listItem);
+  const _EmptyContentsAdv(this.listItem, this.completedQuizzes);
 
   @override
   Widget build(BuildContext context) {
     if (listItem.name == "DummyAdvanced") {
       return Container(
           height: 40.0,
-          child: Text("Advanced Lessons", style: TextStyle(fontSize: 30.0)));
-    } else if (listItem.name == "DummyAdvEnd") {
-      return Divider(color: Colors.black, indent: 20.0, endIndent: 20.0);
+          margin: EdgeInsets.only(left: 10.0),
+          child: Text("Advanced Lessons", style: TextStyle(fontSize: 25.0)));
     } else {
-      return Container(
-        margin: EdgeInsets.only(left: 10.0),
-        height: 40.0,
-        child: Text(listItem.name.toString(), style: TextStyle(fontSize: 20.0)),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(2.0),
-          color: Colors.purple,
+      return Card(
+        margin: EdgeInsets.all(40.0),
+        color: Color.fromARGB(255, 206, 199, 199),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            side: BorderSide(
+              color: Color.fromARGB(255, 111, 109, 109),
+            )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              listItem.name.toString(),
+              style: TextStyle(fontSize: 20.0),
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Builder(builder: (__) {
+                    if (completedQuizzes.length == 0) {
+                      return Icon(Icons.star_border_outlined);
+                    } else {
+                      return Icon(Icons.star_border_outlined,
+                          color: Colors.yellow);
+                    }
+                  }),
+                  Builder(builder: (__) {
+                    if (completedQuizzes.length < 2) {
+                      return Icon(Icons.star_border_outlined);
+                    } else {
+                      return Icon(Icons.star_border_outlined,
+                          color: Colors.yellow);
+                    }
+                  }),
+                  Builder(builder: (__) {
+                    if (completedQuizzes.length < 3) {
+                      return Icon(Icons.star_border_outlined);
+                    } else {
+                      return Icon(Icons.star_border_outlined,
+                          color: Colors.yellow);
+                    }
+                  })
+                ]),
+          ],
         ),
       );
     }
   }
-}
-
-enum _TimelineStatus {
-  done,
-  todo,
 }
